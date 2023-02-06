d153300a894e5a14ec0faae91c729e3d0a53b1c2
Earl Patrick Bellinger <earlbellinger@gmail.com>
2023-02-06 16:04:06 +0100

    added mesa cp

---
+++ shmesa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Convenience tools for MESA
+
+# Earl Patrick Bellinger
+# Max Planck Institute for Astrophysics
+# bellinger@phys.au.dk
+
+mesa_cp () {
+    # Copies a MESA working directory but without
+    # copying LOGS, photos, etc. 
+    SOURCE=#1
+    TARGET=#2
+    rsync -av --progress $SOURCE/ $TARGET \
+          --exclude LOGS \
+          --exclude photos \
+          --exclude .mesa_temp_cache
+}
+
+
+
